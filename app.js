e34cce92d670d1e5edda03bf23dd984a23c6eb8e
Victor Nomwesigwa <nvbethany479@gmail.com>
2019-07-20 18:52:05 +0300

    set up an express server, templating with pug

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "css",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "js",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "js",
+  express.static(path.join(__dirname, "/node_modules/jquery/dist/js"))
+);
+app.set("views", "./src/views");
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index", { list: ["a", "b"] });
+});
+
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
+});

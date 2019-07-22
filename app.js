7cd1609f5fa32ab500ae284f54f72ed5f74705c9
Victor Nomwesigwa <nvbethany479@gmail.com>
2019-07-22 15:01:15 +0300

    signup, signin, protecting routes, passport-local

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,8 +18,16 @@ const nav = [
 
 const bookRouter = require("./src/routes/bookRoutes")(nav);
 const adminRouter = require("./src/routes/adminRoutes")(nav);
+const authRouter = require("./src/routes/authRoutes")(nav);
 
 app.use(morgan("tiny"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: "library" }));
+
+require("./src/config/passport.js")(app);
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   "css",
@@ -35,6 +47,7 @@ app.set("view engine", "ejs");
 
 app.use("/books", bookRouter);
 app.use("/admin", adminRouter);
+app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.render("index", {
